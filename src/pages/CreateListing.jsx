7db694ba87f6abab7740fa6de6a40d8ccba0117c
kfirs127 <62557231+kfirs127@users.noreply.github.com>
2022-10-26 12:20:17 +0300

    fix fomments issue

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -74,10 +74,12 @@ function CreateListing() {
     
         //to add it, need to go over lesson 100
         if (geolocation) {
-          const response = await fetch(
-            `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
-          )
-    
+        //   const response = await fetch(
+        //     `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
+        //   )
+          
+          const response = await fetch("google.com")
+
           const data = await response.json()
     
           gLocation.lat = data.results[0]?.geometry.location.lat ?? 0
